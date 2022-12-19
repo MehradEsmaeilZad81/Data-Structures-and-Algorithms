bad05bf3254f3246107e4e92069967f6d4be9b45
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2022-12-19 21:49:33 +0330

    Define remove function

--- 3. Arrays/main.py
+++ 3. Arrays/main.py
@@ -11,6 +11,16 @@ class Array:
         self.Array[self._index] = value
         self._index += 1
 
+    def remove(self, index):
+        if index < 0 or index > self._index:
+            raise IndexError("Index out of range")
+        for i in range(index, self._index - 1):
+            self.Array[i] = self.Array[i + 1]
+        self.Array[self._index - 1] = None
+        self._index -= 1
+        if self.Array[self.size//2] is None:
+            self.Array = self.Array[:self.size//2]
+
     def __str__(self):
         return str(self.Array)
 
@@ -22,6 +32,10 @@ Array1.insert(2)
 Array1.insert(3)
 Array1.insert(4)
 Array1.insert(5)
+Array1.remove(2)
 Array1.insert(6)
+Array1.insert(7)
+Array1.insert(8)
+Array1.remove(3)
 print(Array1)
-# Output: [1, 2, 3, 4, 5, 6, None, None, None, None]
+# Output: [1, 2, 4, 5, 6, 7, 8, None, None, None]
