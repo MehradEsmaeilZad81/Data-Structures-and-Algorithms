0f0b8ebb879bac0f6c60bf6035a6092181e58dc7
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-01-12 17:04:26 +0330

    Define get method

--- 7. Hast Tables/main.py
+++ 7. Hast Tables/main.py
@@ -11,10 +11,10 @@ class HashTable:
 
     def put(self, key: int, value: str):
         index = self._hash(key)
-        if (entries[index] == None):
-            entries[index] = list()
+        if (self.entries[index] == None):
+            self.entries[index] = list()
 
-        bucket = entries[index]
+        bucket = self.entries[index]
         for entry in bucket:
             if (entry.key == key):
                 entry.value = value
@@ -22,5 +22,26 @@ class HashTable:
 
         bucket.append(self.Entry(key, value))
 
+    def get(self, key: int):
+        index = self._hash(key)
+        bucket = self.entries[index]
+        if bucket:
+            for entry in bucket:
+                if entry.key == key:
+                    return entry.value
+        return None
+
     def _hash(self, key: int):
         return key % self.capacity
+
+
+# Test
+table = HashTable()
+table.put(1, "a")
+table.put(2, "b")
+table.put(3, "c")
+table.put(4, "d")
+table.put(5, "e")
+table.put(6, "f")
+print(table.get(1))
+# Output: a
