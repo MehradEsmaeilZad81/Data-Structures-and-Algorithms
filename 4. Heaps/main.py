f22e96f180090cce02567de3e48449ba686434c8
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-01-21 18:38:47 +0330

    Define swap, parent function

--- 4. Heaps/main.py
+++ 4. Heaps/main.py
@@ -8,8 +8,14 @@ class Heap:
         self.size += 1
 
         index = size - 1
-        parentIndex = (index - 1) // 2
+        parentIndex = self._parent(index)
         while items[index] > items[parentIndex]:
-            items[index], items[parentIndex] = items[parentIndex], items[index]
+            self._swap(index, parentIndex)
             index = parentIndex
-            parentIndex = (index - 1) // 2
+            parentIndex = self._parent(index)
+
+    def _swap(self, index1, index2):
+        items[index1], items[index2] = items[index2], items[index1]
+
+    def _parent(self, index):
+        return (index - 1) // 2
