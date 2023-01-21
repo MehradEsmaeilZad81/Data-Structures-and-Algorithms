4d36599a0b3b7873fa519850eae4b34f264ed740
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-01-21 18:48:06 +0330

    Make Test

--- 4. Heaps/main.py
+++ 4. Heaps/main.py
@@ -6,7 +6,7 @@ class Heap:
     def insert(self, value):
         if self._isFull():
             self._resize()
-        items[self.size] = value
+        self.items[self.size] = value
         self.size += 1
         self._bubbleUp()
 
@@ -17,16 +17,33 @@ class Heap:
         self.items = newItems
 
     def _isFull(self):
-        return self.size == len(items)
+        return self.size == len(self.items)
 
     def _bubbleUp(self):
-        index = size - 1
-        while index > 0 and items[index] > items[self._parent(index)]:
+        index = self.size - 1
+        while index > 0 and self.items[index] > self.items[self._parent(index)]:
             self._swap(index, self._parent(index))
             index = self._parent(index)
 
     def _swap(self, index1, index2):
-        items[index1], items[index2] = items[index2], items[index1]
+        self.items[index1], self.items[index2] = self.items[index2], self.items[index1]
 
     def _parent(self, index):
         return (index - 1) // 2
+
+    def __str__(self):
+        return str(self.items[:self.size])
+
+
+# Test
+heap = Heap()
+heap.insert(10)
+heap.insert(5)
+heap.insert(17)
+heap.insert(4)
+heap.insert(20)
+heap.insert(19)
+heap.insert(3)
+
+print(heap)
+# Output: [20, 19, 17, 4, 10, 5, 3]
