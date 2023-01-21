0295d5a7d85f0bd93a7fb97e5d803aeeec45cdf4
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-01-21 17:50:04 +0330

    Implement rotations

--- 3. AVL Trees/main.py
+++ 3. AVL Trees/main.py
@@ -27,23 +27,45 @@ class AVLTree:
         else:
             root._rightChild = self._insert(root._rightChild, value)
 
-        root._height = max(self._height(root._leftChild),
-                           self._height(root._rightChild)) + 1
+        self._setHeight(node=root)
 
-        self._balance(root)
-        return root
+        return self._balance(root)
 
     def _balance(self, root):
         if self._isLeftHeavy(root):
             if (self._balanceFactor(root._leftChild) < 0):
-                # root._leftChild = self._rotateLeft(root._leftChild)
-                print("Left Rotation: ", root._leftChild._value)
-            print("Right Rotation: ", root._value)
+                root._leftChild = self._rotateLeft(root._leftChild)
+            return self._rotateRight(root)
         elif self._isRightHeavy(root):
             if (self._balanceFactor(root._rightChild) > 0):
-                # root._rightChild = self._rotateRight(root._rightChild)
-                print("Right Rotation: ", root._rightChild._value)
-            print("Left Rotation: ", root._value)
+                root._rightChild = self._rotateRight(root._rightChild)
+            return self._rotateLeft(root)
+
+        return root
+
+    def _rotateLeft(self, root):
+        newRoot = root._rightChild
+        root._rightChild = newRoot._leftChild
+        newRoot._leftChild = root
+
+        self._setHeight(node=root)
+        self._setHeight(node=newRoot)
+
+        return newRoot
+
+    def _rotateRight(self, root):
+        newRoot = root._leftChild
+        root._leftChild = newRoot._rightChild
+        newRoot._rightChild = root
+
+        self._setHeight(node=root)
+        self._setHeight(node=newRoot)
+
+        return newRoot
+
+    def _setHeight(self, node):
+        node._height = max(self._height(node._leftChild),
+                           self._height(node._rightChild)) + 1
 
     def _isLeftHeavy(self, node):
         return self._balanceFactor(node) > 1
