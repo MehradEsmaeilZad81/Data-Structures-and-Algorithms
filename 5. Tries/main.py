e1552b25681f8fec51ada7e36fbd3f6a80a91de4
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-01-25 01:00:00 +0330

    Make Test

--- 5. Tries/main.py
+++ 5. Tries/main.py
@@ -1,16 +1,17 @@
 class Trie:
-    def __init__(self):
-        self.root = Node(" ")
 
     class Node:
         def __init__(self, value: str):
             self.ALPHABET_SIZE = 26
             self.value = value
-            self.children = [None] * ALPHABET_SIZE
+            self.children = [None] * self.ALPHABET_SIZE
             self.word_finished = False
 
         def __str__(self):
             return "Node{" + "value=" + self.value + "}"
+
+    def __init__(self):
+        self.root = self.Node("")
 
     def insert(self, word: str):
         node = self.root
@@ -20,3 +21,12 @@ class Trie:
                 node.children[index] = self.Node(char)
             node = node.children[index]
         node.word_finished = True
+
+
+# Test
+trie = Trie()
+trie.insert("hello")
+trie.insert("world")
+trie.insert("hell")
+trie.insert("heaven")
+print("Done")
