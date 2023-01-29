e34621584768323d5e7560ad33ab30b4597e9400
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-01-29 17:52:31 +0330

    Define getShortestPath function

--- 7. Undirected Graphs/main.py
+++ 7. Undirected Graphs/main.py
@@ -1,3 +1,14 @@
+class Path:
+    def __init__(self):
+        self.nodes = list()
+
+    def add(self, node: str):
+        self.nodes.append(node)
+
+    def __str__(self):
+        return " -> ".join(self.nodes)
+
+
 class WeightedGraph:
 
     class Node:
@@ -35,7 +46,7 @@ class WeightedGraph:
         self.adjacencyList[toNode].append(
             self.Edge(toNode, fromNode, weight))
 
-    def getShortestDistance(self, label1, label2):
+    def getShortestPath(self, label1, label2):
         fromNode = self.nodes[label1]
         toNode = self.nodes[label2]
         distances = dict()
@@ -43,6 +54,7 @@ class WeightedGraph:
             distances[node] = float("inf")
         distances[fromNode] = 0
 
+        previousNodes = dict()
         visited = set()
         queue = list()
         queue.append(fromNode)
@@ -55,9 +67,24 @@ class WeightedGraph:
                 newDistance = distances[node] + edge.weight
                 if newDistance < distances[edge.toNode]:
                     distances[edge.toNode] = newDistance
+                    previousNodes[edge.toNode] = node
                 queue.append(edge.toNode)
 
-        return distances[toNode]
+        return self._buildPath(previousNodes, toNode, fromNode)
+
+    def _buildPath(self, previousNodes, toNode, fromNode):
+        stack = list()
+        stack.append(toNode)
+        previous = previousNodes[toNode]
+        while previous != fromNode:
+            stack.append(previous)
+            previous = previousNodes[previous]
+        stack.append(fromNode)
+        path = Path()
+        while len(stack) > 0:
+            path.add(stack.pop().label)
+
+        return path
 
     def print(self):
         for fromNode, edges in self.adjacencyList.items():
@@ -78,7 +105,8 @@ graph.addEdge("B", "D", 30)
 graph.addEdge("C", "D", 40)
 
 graph.print()
-print("A --> D",graph.getShortestDistance("A", "D"))
-print("A --> B",graph.getShortestDistance("A", "B"))
-print("B --> D",graph.getShortestDistance("B", "D"))
-print("C --> D",graph.getShortestDistance("C", "D"))
+print(graph.getShortestPath("A", "D"))
+print(graph.getShortestPath("A", "B"))
+print(graph.getShortestPath("B", "D"))
+print(graph.getShortestPath("C", "D"))
+print(graph.getShortestPath("D", "A"))
